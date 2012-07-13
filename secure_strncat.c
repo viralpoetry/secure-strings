0d313d6c0587b773ec255273525fdb007fa5d581
Peter Gasper <gaspinkoo@gmail.com>
2012-07-13 17:58:35 +0300

    Slovak comments removed.

--- secure_strncat.c
+++ secure_strncat.c
@@ -1,10 +1,5 @@
-
 #include <stdio.h>
 
-// PARAMETRE:
-// secure_strncat( cieæovÈ pole, 
-//			    veækosù cieæovÈho poæa, 
-//			    reùazec ktor˝ sa m· kopÌrovaù )
 
 char * secure_strncat(char * dst, size_t sizeDst, const char * src)
 {
@@ -13,16 +8,17 @@ char * secure_strncat(char * dst, size_t sizeDst, const char * src)
     
     if (celkovaDlzka < sizeDst)
     {
-	   // skopÌruj zdrojov˝ reùazec do cieæovÈho poæa
+
 	   return strncat(dst, src, dlzkaZdroja);
     }
     else
-	   // inak vr·ù nezmenenÈ cieæovÈ pole
+
 	   return dst;
 }
 
 int main(int argc, char *argv[])
 {
+    /* Usage */
     char Array[10] = "0123";
     char String[]  = "45678";
 
